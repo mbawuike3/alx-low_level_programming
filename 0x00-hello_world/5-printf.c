64b49c9822c2757fe33d5f63b53a0069cbf5adc6
Mbawuike Stanley <mbawuikestanley@gmail.com>
2022-03-10 19:08:19 +0000

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  *
- * main - prints a sentence with the printf function
+ * main - Entry point
  *
- * Return: returns 0 after printing
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
 	return (0);
 }
