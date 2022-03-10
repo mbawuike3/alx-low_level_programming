70591eb005f076a0a3cfc2cdfa05df91aa2260e0
Mbawuike Stanley <mbawuikestanley@gmail.com>
2022-03-10 18:41:51 +0000

    Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - write to stdout
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
